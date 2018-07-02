6b45f24d485e50bd5b01b998ef38d42d98021d0e
Irhad <irhadhalilovic@hotmail.com>
2018-07-03 00:34:14 +0200

    spring boot application created

---
+++ src/test/java/com/irhad/restwebshop/RestWebShopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.irhad.restwebshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestWebShopApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
